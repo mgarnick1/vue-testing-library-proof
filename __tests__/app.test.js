6e599752b0886d1bbe7e9d714f1b96e4815cd6b4
Mark <mgarnick@techtonicgroup.com>
2020-10-09 16:07:27 -0600

    added a button click event test that passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-undef */
 import App from "../src/App.vue";
-import { render } from "@testing-library/vue";
+import { fireEvent, render } from "@testing-library/vue";
 
 test("It renders correctly", () => {
   const { getByText } = render(App);
   getByText("Times clicked: 0");
+});
+
+test("increment the count", async () => {
+  const { getByText } = render(App);
+  const increment = getByText("increment");
+  await fireEvent.click(increment);
+  getByText("Times clicked: 1");
 });
